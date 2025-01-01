710e38d31806bac1a52f791148ee62767fae05b1
syunado <bennettbros68@gmail.com>
2024-12-31 23:46:36 -0500

    section 6 is done, built a form with server side logging, very cool stuff, last section up next

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,20 @@
+
 export default function Page(){
+  async function submitForm(formData) {
+    "use server";
+    const formFields = {
+      email: formData.get("email"),
+      message: formData.get("message")
+    };
+    console.log("formFields", formFields);
+    console.log("TODO: Send these form field values to a backend");
+    return formFields;
+  }
   return(
     <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
       <div>
         <h1 className="text-2xl font-bold text-center mb-6">Contact Us!</h1>
-        <form className="space-y-4">
+        <form className="space-y-4" action={submitForm}>
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
             <input 
